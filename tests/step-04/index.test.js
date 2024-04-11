dbabe471fdb5cd5280b25c87f266276b7f8152ca
lata <imlata1111@gmail.com>
2024-04-12 03:14:28 +0530

    added tets 4

--- tests/step-04/index.test.js
+++ tests/step-04/index.test.js
@@ -6,8 +6,8 @@ test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+    expect(data[0].name).toBe('Lata');
+    expect(data[0].age).toBe('19'); //ignore the string type here, we will fix this later
 });
 
 test('Parse SQL Query', () => {
@@ -26,5 +26,5 @@ test('Execute SQL Query', async () => {
     expect(result[0]).toHaveProperty('id');
     expect(result[0]).toHaveProperty('name');
     expect(result[0]).not.toHaveProperty('age');
-    expect(result[0]).toEqual({ id: '1', name: 'John' });
+    expect(result[0]).toEqual({ id: '1', name: 'Lata' });
 });
