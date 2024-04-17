903ab795460063c4f349672b034cf766c4a52c12
lata <imlata1111@gmail.com>
2024-04-18 01:46:39 +0530

    done test 6

--- tests/step-05/index.test.js
+++ tests/step-05/index.test.js
@@ -6,8 +6,8 @@ test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
-    expect(data[0].name).toBe('Lata');
-    expect(data[0].age).toBe('19'); //ignore the string type here, we will fix this later
+    expect(data[0].name).toBe('John');
+    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });
 
 test('Parse SQL Query', () => {
@@ -16,7 +16,7 @@ test('Parse SQL Query', () => {
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'sample',
-        whereClause: null
+        whereClauses: []
     });
 });
 
@@ -27,7 +27,7 @@ test('Execute SQL Query', async () => {
     expect(result[0]).toHaveProperty('id');
     expect(result[0]).toHaveProperty('name');
     expect(result[0]).not.toHaveProperty('age');
-    expect(result[0]).toEqual({ id: '1', name: 'Lata' });
+    expect(result[0]).toEqual({ id: '1', name: 'John' });
 });
 
 test('Parse SQL Query with WHERE Clause', () => {
@@ -36,7 +36,11 @@ test('Parse SQL Query with WHERE Clause', () => {
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'sample',
-        whereClause: 'age = 25'
+        whereClauses: [{
+          field: "age",
+          operator: "=",
+          value: "25",
+        }],
     });
 });
 
